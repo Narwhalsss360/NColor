ca2bd570900a6d52a7838f17da4c85f873114cf2
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-05-18 15:37:23 -0400

    Updated examples, keywords and version

--- examples/NColor/NColor.ino
+++
@@ -1,9 +0,0 @@
-#include <NColor.h>
-
-void setup()
-{
-}
-
-void loop()
-{
-}
--- examples/RGBLed/RGBLed.ino
+++ examples/RGBLed/RGBLed.ino
@@ -1,9 +1,22 @@
 #include <NColor.h>
 
+#define RED 7
+#define GREEN 8
+#define BLUE 9
+
+ColorLED led = ColorLED(RED, GREEN, BLUE)
+
 void setup()
 {
 }
 
 void loop()
 {
+    HSV hsv = (HSV)led;
+    if (hsv.hue == 360)
+        hsv.hue = 0;
+    else
+        hsv.hue += 1;
+    led = hsv;
+    delay(125);
 }
